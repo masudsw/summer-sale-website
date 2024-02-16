f1e3abfc793028d8a39a8774df0594d38e586f27
Golam Mustafa <golammustafa@gmail.com>
2024-02-16 16:22:03 +0600

    script addedd for discount

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const cards=document.getElementsByClassName('card');
 console.log(cards);
 let total=0;
+let index=1;
+let totalPrice;
 for(let card of cards){
     card.addEventListener('click',function(){
         let h3=card.querySelector('h3');
@@ -8,16 +10,39 @@ for(let card of cards){
         let price=card.querySelector('span');
         price=price.innerText.slice(2);
         price=parseFloat(price);
-        console.log(price);
         const titleContainer=document.getElementById('title-container');
         const p=document.createElement('p');
-        p.innerText=item;
+        p.innerText=index + '. ' + item;
+        index++;
+        console.log(index);
         titleContainer.appendChild(p);
         total=total+price;
-        let totalPrice=document.getElementById('totalPrice');
+        totalPrice= document.getElementById('totalPrice');
         totalPrice.innerText=total;
     })
-  
 }
+
+const applyButton=document.getElementById('apply-btn');
+applyButton.addEventListener('click',function(){
+    let inputDiscount=document.getElementById('input-field').value;
+    inputDiscount=inputDiscount.replaceAll(" ", "");
+
+    if(inputDiscount.toUpperCase()==='SELL200' && (totalPrice.innerText>=200))
+    {
+        // 20% discount
+        const disCount=totalPrice.innerText*0.2;
+        document.getElementById('discountPrice').innerText=disCount;
+        document.getElementById('total').innerText=totalPrice.innerText-disCount;
+    }
+    else
+    {
+        alert('You are not qualified for discount');
+        document.getElementById('total').innerText=totalPrice.innerText;
+
+    }
+    inputDiscount.value='';
+
+})
+
 
 
